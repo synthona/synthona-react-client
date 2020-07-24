1474e7b8488efd2dbf3e45ec587fcc6883ce28ca
geographicscard <>
2020-07-23 22:40:07 -0500

    added additional UI messages for imports and exports

--- src/api/redux/actions/portActions.js
+++ src/api/redux/actions/portActions.js
@@ -5,7 +5,7 @@ import { message } from 'antd';
 
 // export the instance data and get the url of the file
 export const generateInstanceExport = () => async (dispatch) => {
-  message.success('generating...', 2);
+  message.success('generating export...', 2);
   try {
     dispatch({ type: GENERATE_EXPORT });
     const response = await instance.put('/port/export/all');
@@ -24,7 +24,7 @@ export const generateInstanceExport = () => async (dispatch) => {
 
 // generate export based on a node
 export const generateExportByUUID = (uuid) => async (dispatch) => {
-  message.success('generating...', 2);
+  message.success('generating export...', 2);
   try {
     dispatch({ type: GENERATE_EXPORT });
     const response = await instance.put('/port/export/', { uuid });
@@ -43,6 +43,7 @@ export const generateExportByUUID = (uuid) => async (dispatch) => {
 
 // export the instance data and get the url of the file
 export const unpackSynthonaImport = (uuid) => async (dispatch) => {
+  message.success('generating imports...', 1);
   try {
     const response = await instance.put('/port/import/', { uuid });
     if (response.status === 200) {
@@ -57,6 +58,7 @@ export const unpackSynthonaImport = (uuid) => async (dispatch) => {
 };
 
 export const removeSynthonaImportsByPackage = (uuid) => async (dispatch) => {
+  message.success('removing imports...', 1);
   try {
     const response = await instance.patch('/port/export/undo/', { uuid });
     if (response.status === 200) {
