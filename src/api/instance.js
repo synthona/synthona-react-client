7e7a9446623ae378b88b412e82876f6b578e9087
geographicscard <63616362+synthona@users.noreply.github.com>
2021-01-21 17:27:55 -0600

    changed default instance port to match current server config

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const CancelToken = axios.CancelToken;
 const source = CancelToken.source();
 export default axios.create({
-  baseURL: 'http://localhost:9001',
+  baseURL: 'http://localhost:9004',
   withCredentials: true,
   cancelToken: source.token,
 });
