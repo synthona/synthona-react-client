10ec885b879c7a3d71047a7e4a85e97f986810e6
geographicscard <63616362+synthona@users.noreply.github.com>
2021-02-22 02:50:51 -0600

    added emoji back to login screen

--- src/components/layouts/Login.js
+++ src/components/layouts/Login.js
@@ -43,7 +43,24 @@ class Login extends Component {
             >
               {({ isSubmitting }) => (
                 <Form className='login-form'>
-                  <h1 className='login-title'>{this.getUsername()} </h1>
+                  <h1 className='login-title'>
+                    <span
+                      role='img'
+                      aria-label='jsx-a11y/accessible-emoji'
+                      style={{ paddingRight: '0.5rem' }}
+                    >
+                      {' '}
+                      ✨
+                    </span>
+                    synthona
+                    <span
+                      role='img'
+                      aria-label='jsx-a11y/accessible-emoji'
+                      style={{ paddingRight: '0.5rem' }}
+                    >
+                      {'  '}✨
+                    </span>
+                  </h1>
                   <Field type='email' name='email' placeholder='email' className='login-field' />
                   <ErrorMessage name='email' component='div' className='login-error' />
                   <Field
