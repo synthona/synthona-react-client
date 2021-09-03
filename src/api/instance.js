7fac49860c22303ebd19382196e989755895c5d4
geographicscard <63616362+synthona@users.noreply.github.com>
2021-09-03 15:05:56 -0500

    temporarily switch the baseUrl back for the req instance for 1.2.5 build

--- src/api/instance.js
+++ src/api/instance.js
@@ -3,8 +3,8 @@ const CancelToken = axios.CancelToken;
 const source = CancelToken.source();
 
 export default axios.create({
-	// baseURL: 'http://localhost:3077',
-	baseURL: 'http://' + window.location.hostname + ':3077',
+	baseURL: 'http://localhost:3077',
+	// baseURL: 'http://' + window.location.hostname + ':3077',
 	withCredentials: true,
 	cancelToken: source.token,
 });
