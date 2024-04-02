ddfae4e07b3f2b4fd69798d793ea8b60b9663b6a
geographicscard <synthona@gmail.com>
2024-04-01 22:34:47 -0500

    clear scrollY stored position when you go to the homepage

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Layout } from 'antd';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Layout } from "antd";
 // custom code
-import MainSider from '../elements/MainSider';
-import IOBar from '../elements/IOBar';
-import NodeList from '../elements/node/NodeList';
+import MainSider from "../elements/MainSider";
+import IOBar from "../elements/IOBar";
+import NodeList from "../elements/node/NodeList";
 
 const { Content } = Layout;
 
 class Home extends Component {
 	componentDidMount() {
-		document.title = 'home';
+		document.title = "home";
 		// temporary fix to undo whatever is setting overflow hidden on-login
-		document.body.style.removeProperty('overflow');
-		window.scrollTo({ top: 0, behavior: 'smooth' });
+		document.body.style.removeProperty("overflow");
+		window.scrollTo({ top: 0, behavior: "smooth" });
+		// clear stored quill editor scrollposition if there is one
+		localStorage.removeItem("quillScrollY");
 	}
 
 	renderMainSider = () => {
@@ -24,12 +26,12 @@ class Home extends Component {
 
 	render() {
 		return (
-			<Layout className='page-layout'>
+			<Layout className="page-layout">
 				<Layout>
 					{this.renderMainSider()}
-					<Content style={{ minHeight: '100vh' }}>
+					<Content style={{ minHeight: "100vh" }}>
 						<IOBar />
-						<div className='list-container'>
+						<div className="list-container">
 							<NodeList />
 						</div>
 					</Content>
@@ -42,7 +44,7 @@ class Home extends Component {
 const mapStateToProps = (state) => {
 	return {
 		isSignedIn: state.auth.isSignedIn,
-		mainSider: state.components.componentList['mainSider'],
+		mainSider: state.components.componentList["mainSider"],
 	};
 };
 
